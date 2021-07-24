19c42fa62ff4e1e5613cd451c0bea5d0c3784824
Zenón Burgos <znburgos@gmail.com>
2021-07-23 21:59:04 -0600

    ConfirmAppointment

--- app/src/main/java/solucionesnegocios/com/CreateAppointmentActivity.kt
+++ app/src/main/java/solucionesnegocios/com/CreateAppointmentActivity.kt
@@ -2,10 +2,23 @@ package solucionesnegocios.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_appointment.*
 
 class CreateAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_appointment)
+
+        btnNext.setOnClickListener {
+            cvStep1.visibility = View.GONE
+            cvStep2.visibility = View.VISIBLE
+        }
+
+        btnConfirmAppointment.setOnClickListener {
+            Toast.makeText(this, "Cita registrada correctamente", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
